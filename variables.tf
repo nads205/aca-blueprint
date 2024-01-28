bc04b838bc5336e2619b6a0d31fc22df1f149c07
Naadir Akhtar <naadirakhtar@hotmail.com>
2024-01-28 19:23:57 +0000

    tags and change name

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aca_name" {
-  default     = "blueprintaca4321"
+  default     = "cmsdev002"
   type        = string
   description = "Name for Azure Container App"
 }
@@ -9,3 +9,37 @@ variable "location" {
   description = "Location of Azure resources"
 }
 
+## TAGS
+variable "tags" {
+  type = object({
+    ApplicationService : string
+    BusinessOwner : string
+    BusinessUnit : string
+    DataClassification : string
+    Environment : string
+    IRCode : string
+    OU : string
+    ProjectCodeOrCostCentre : string
+    ServiceTier : string
+    SupportInfoAppTeam : string
+    SupportInfoInfTeam : string
+    TaskOrGLCode : string
+    TechnicalOwner : string
+  })
+  description = "Tags to apply"
+  default = {
+    ApplicationService : "airtricitycmsdev"
+    BusinessOwner : "Robbie.Holden@sse.com"
+    BusinessUnit : "ECS"
+    DataClassification : "Public"
+    Environment : "DEV"
+    IRCode : "N/A"
+    OU : "N/A"
+    ProjectCodeOrCostCentre : "2486"
+    ServiceTier : "Not Defined"
+    SupportInfoAppTeam : "HCL_Cloud_&_Infrastructure_Delivery_Support"
+    SupportInfoInfTeam : "HCL_Cloud_&_Infrastructure_Delivery_Support"
+    TaskOrGLCode : "RITM2043111"
+    TechnicalOwner : "Naadir.Akhtar@sse.com"
+  }
+}
